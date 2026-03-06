bca37124a68e333dc277104003027b3e94173741
你的GitHub用户名 <jzjaydee@gmail.com>
2026-03-06 23:37:27 +0800

    Fix build warnings: add missing icon and eslint config

--- src/resources/icons.ts
+++ src/resources/icons.ts
@@ -20,6 +20,7 @@ import {
   PiGridFourDuotone,
   PiBookBookmarkDuotone,
   PiImageDuotone,
+  PiKeyDuotone,
 } from "react-icons/pi";
 
 import { FaDiscord, FaGithub, FaLinkedin, FaX, FaThreads } from "react-icons/fa6";
@@ -36,6 +37,7 @@ export const iconLibrary: Record<string, IconType> = {
   calendar: HiCalendarDays,
   home: PiHouseDuotone,
   gallery: PiImageDuotone,
+  key: PiKeyDuotone,
   discord: FaDiscord,
   eye: HiOutlineEye,
   eyeOff: HiOutlineEyeSlash,
